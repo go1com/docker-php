3583e0429197c0a08a76fe57f32bed10fc4d90ea
Sang Le <sang@go1.com.au>
2017-05-05 10:28:27 +0700

    Allow run custom hook first

--- rootfs/start.sh
+++ rootfs/start.sh
@@ -10,10 +10,6 @@ sed -i "s#^DocumentRoot \".*#DocumentRoot \"/$webroot\"#g" /etc/apache2/httpd.co
 sed -i "s#/var/www/localhost/htdocs#/$webroot#" /etc/apache2/httpd.conf
 printf "\n<Directory \"/$webroot\">\n\tAllowOverride All\n</Directory>\n" >> /etc/apache2/httpd.conf
 
-# Always chown webroot for better mounting
-mkdir -p $webroot
-chown -Rf apache.www-data $webroot
-
 # Allow run custom script
 if [ ! -z "$SCRIPT" ] && [ -f "$SCRIPT" ]; then
   chmod a+x $SCRIPT
@@ -23,5 +19,9 @@ fi
 if [ -f /app/resources/docker/hook-start ]; then
     source /app/resources/docker/hook-start
 fi
+
+# Always chown webroot for better mounting
+mkdir -p $webroot
+chown -Rf apache.www-data $webroot
 
 httpd -D FOREGROUND
